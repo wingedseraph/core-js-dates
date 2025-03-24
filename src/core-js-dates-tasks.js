bb24fd0b482a4b98ff3f3aa9f2f522ca24382661
wingedseraph <r89647@gmail.com>
2025-03-24 23:34:41 +0300

    feat: 2 more functions

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -95,8 +95,8 @@ function getNextFriday(date) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  return new Date(year, month, 0).getDate();
 }
 
 /**
@@ -110,8 +110,10 @@ function getCountDaysInMonth(/* month, year */) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const first = new Date(dateStart);
+  const second = new Date(dateEnd);
+  return (second - first) / (1000 * 60 * 60 * 24) + 1;
 }
 
 /**
