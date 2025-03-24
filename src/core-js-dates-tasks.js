08b5d361941c3828cd19e2178e56c0514c45fcdd
wingedseraph <r89647@gmail.com>
2025-03-24 23:02:50 +0300

    feat: 2 more functions

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -50,8 +50,17 @@ function getTime(date) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  return days[new Date(date).getDay()];
 }
 
 /**
@@ -65,8 +74,14 @@ function getDayName(/* date */) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const startDate = new Date(date);
+  const daysToFriday = (5 - startDate.getDay() + 7) % 7 || 7;
+
+  const nextFriday = new Date(startDate);
+  nextFriday.setUTCDate(startDate.getDate() + daysToFriday);
+  nextFriday.setUTCHours(0, 0, 0, 0);
+  return nextFriday;
 }
 
 /**
